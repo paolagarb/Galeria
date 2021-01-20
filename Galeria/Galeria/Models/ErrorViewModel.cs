b2022c8cfafc734a88d6a60727f722871ec36a7c
Paola Garbato <paolagarbato@gmail.com>
2021-01-20 17:15:35 -0300

    Iniciando o projeto

---
+++ Galeria/Galeria/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Galeria.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
