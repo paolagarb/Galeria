6da1361587109d468c5c157740c5f4c7f47a4d4c
Paola Garbato <paolagarbato@gmail.com>
2021-01-20 18:18:26 -0300

    Controller e View de álbuns, Migration SQL Server

---
+++ Galeria/Galeria/Controllers/AlbunsController.cs
@@ -0,0 +1,158 @@
+﻿using Galeria.Data;
+using Galeria.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Galeria.Controllers
+{
+    public class AlbunsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AlbunsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Albuns
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Albuns.Include(a => a.IdentityUser);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Albuns/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Albuns
+                .Include(a => a.IdentityUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            return View(album);
+        }
+
+        // GET: Albuns/Create
+        public IActionResult Create()
+        {
+            ViewData["IdentityUserId"] = new SelectList(_context.MyProperty, "Id", "Id");
+            return View();
+        }
+
+        // POST: Albuns/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,IdentityUserId")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(album);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdentityUserId"] = new SelectList(_context.MyProperty, "Id", "Id", album.IdentityUserId);
+            return View(album);
+        }
+
+        // GET: Albuns/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Albuns.FindAsync(id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdentityUserId"] = new SelectList(_context.MyProperty, "Id", "Id", album.IdentityUserId);
+            return View(album);
+        }
+
+        // POST: Albuns/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,IdentityUserId")] Album album)
+        {
+            if (id != album.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(album);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlbumExists(album.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdentityUserId"] = new SelectList(_context.MyProperty, "Id", "Id", album.IdentityUserId);
+            return View(album);
+        }
+
+        // GET: Albuns/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Albuns
+                .Include(a => a.IdentityUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            return View(album);
+        }
+
+        // POST: Albuns/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var album = await _context.Albuns.FindAsync(id);
+            _context.Albuns.Remove(album);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AlbumExists(int id)
+        {
+            return _context.Albuns.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Galeria/Galeria/Data/Migrations/20210120211731_sql-GaleriaPessoal.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Galeria.Data.Migrations
+{
+    public partial class sqlGaleriaPessoal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
